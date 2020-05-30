8ce08de04ad899a2950fe975a4fa7fd43fa0d8ac
ChiragDJ11 <60982821+ChiragDJ11@users.noreply.github.com>
2020-05-30 12:26:13 +0530

    Flask File added

---
+++ WQP-Flask/model.py
@@ -0,0 +1,52 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+data=pd.read_csv('Water.csv',encoding='ISO-8859-1',low_memory=False)
+
+#Data Preprocessing
+data['STATION CODE'].fillna(data['STATION CODE'].mean(),inplace=True)
+data['PH'].fillna(data['PH'].mean(),inplace=True)
+data['CONDUCTIVITY (µmhos/cm)'].fillna(data['CONDUCTIVITY (µmhos/cm)'].mean(),inplace=True)
+data['D.O. (mg/l)'].fillna(data['D.O. (mg/l)'].mean(),inplace=True)
+x=['LOCATIONS','STATE']
+data[x]=data[x].fillna(data.mode().iloc[0])
+data['B.O.D. (mg/l)'] = pd.to_numeric(data['B.O.D. (mg/l)'],errors='coerce')
+data['NITRATENAN N+ NITRITENANN (mg/l)'] = pd.to_numeric(data['NITRATENAN N+ NITRITENANN (mg/l)'],errors='coerce')
+data['FECAL COLIFORM (MPN/100ml)'] = pd.to_numeric(data['FECAL COLIFORM (MPN/100ml)'],errors='coerce')
+data['TOTAL COLIFORM (MPN/100ml)Mean'] = pd.to_numeric(data['TOTAL COLIFORM (MPN/100ml)Mean'],errors='coerce')
+data['TOTAL COLIFORM (MPN/100ml)Mean'].fillna(60, inplace=True)
+data['FECAL COLIFORM (MPN/100ml)'].fillna(17, inplace=True)
+data['NITRATENAN N+ NITRITENANN (mg/l)'].fillna(1.42, inplace=True)
+data['B.O.D. (mg/l)'].fillna(1.5, inplace=True)
+data['Temp'] = pd.to_numeric(data['Temp'],errors='coerce')
+data['Temp'].fillna(30,inplace=True)
+
+
+X=data.iloc[:,3:11].values
+Y=data.iloc[:,-1].values
+
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
+
+from sklearn.ensemble import RandomForestRegressor
+rig=RandomForestRegressor(n_estimators=100,random_state=0)
+
+#Fitting model with trainig data
+rig.fit(X,Y)
+
+# Saving model to disk
+pickle.dump(rig, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+
+a=model.predict([[27.2,5.6,200.0,1.5,0.1,3000.1,5000.5,2014]])
+if 6.6<a<7.4:
+    print("Water is Portable with PH value :",a)
+else:
+    print("Contaminated water with PH value :",a)
+
+
---
+++ WQP-Flask/request.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'Temp':28.7, 'D.O. (mg/l)':6.4, 'CONDUCTIVITY (µmhos/cm)':189, 'B.O.D. (mg/l)':2,'NITRATENAN N+ NITRITENANN (mg/l)':0.1,'FECAL COLIFORM (MPN/100ml)':4500, 'TOTAL COLIFORM (MPN/100ml)Mean':5500, 'year':2014})
+
+print(r.json())
+
+
+
